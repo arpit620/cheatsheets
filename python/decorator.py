0c89abb28d4a988e3a95ca19474bec91679fdc4a
Arpit Gupta <arpit.gupta@walmartlabs.com>
2020-07-18 09:43:27 +0530

    Decorator with argument

--- python/decorator.py
+++ python/decorator.py
@@ -86,3 +86,48 @@ def display_info(name, age):
 
 display_info('Tom', 22)
 
+######################################################################
+
+# Decorators With arguments:
+
+# Before 
+
+def decorator_function(original_function):
+    def wrapper_function(*args, **kwargs):
+        print('Executed Before', original_function.__name__)
+        result = original_function(*args, **kwargs)
+        print('Executed After', original_function.__name__, '\n')
+        return result
+    return wrapper_function
+
+
+@decorator_function
+def display_info(name, age):
+    print('display_info ran with arguments ({}, {})'.format(name, age))
+
+
+display_info('John', 25)
+display_info('Travis', 30)
+
+
+# After
+
+def prefix_decorator(prefix):
+    def decorator_function(original_function):
+        def wrapper_function(*args, **kwargs):
+            print(prefix, 'Executed Before', original_function.__name__)
+            result = original_function(*args, **kwargs)
+            print(prefix, 'Executed After', original_function.__name__, '\n')
+            return result
+        return wrapper_function
+    return decorator_function
+
+
+@prefix_decorator('LOG:')
+def display_info(name, age):
+    print('display_info ran with arguments ({}, {})'.format(name, age))
+
+
+display_info('John', 25)
+display_info('Travis', 30)
+
